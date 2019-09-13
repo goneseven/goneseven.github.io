fc1c81c12a025821c04d2aa4f78fdcb3a078b676
goneseven <55253758+goneseven@users.noreply.github.com>
2019-09-13 14:14:39 +0100

    Update verbs_generator.py

--- verbs_generator.py
+++ verbs_generator.py
@@ -1,19 +1,23 @@
 import urllib.request
 import xml.etree.ElementTree as ET
 
-list = []
-sources = ['https://trends.google.com/trends/trendingsearches/daily/rss?geo=US',
-    'https://trends.google.com/trends/trendingsearches/daily/rss?geo=GB',
-    'https://trends.google.com/trends/trendingsearches/daily/rss?geo=IN',
-    'https://trends.google.com/trends/trendingsearches/daily/rss?geo=DE']
+def build_verbs():
+    list = []
+    sources = ['https://trends.google.com/trends/trendingsearches/daily/rss?geo=US',
+        'https://trends.google.com/trends/trendingsearches/daily/rss?geo=GB',
+        'https://trends.google.com/trends/trendingsearches/daily/rss?geo=IN',
+        'https://trends.google.com/trends/trendingsearches/daily/rss?geo=DE']
 
-for src in sources:
-    contents = urllib.request.urlopen(src).read()
-    root = ET.ElementTree(ET.fromstring(contents)).getroot()
-    for tag in root.iterfind('channel/item/title'):
-        list.append(tag.text)
+    for src in sources:
+        contents = urllib.request.urlopen(src).read()
+        root = ET.ElementTree(ET.fromstring(contents)).getroot()
+        for tag in root.iterfind('channel/item/title'):
+            list.append(tag.text)
 
-print(len(list), list)
-   
-with open('verbs.html', 'a') as f:
-    f.write(",".join(list)+',')
+    print(len(list), list)
+        
+    with open('verbs.html', 'a') as f:
+        f.write(",".join(list)+',')
+
+if __name__ == '__main__':
+    build_verbs()
